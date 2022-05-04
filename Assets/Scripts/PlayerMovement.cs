f5927c06d19eefc5cc2ea1161a14a5e92133a03a
shayoran <gomesfrancisco81@gmail.com>
2022-05-04 23:57:22 +0100

    The Environment

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float turnSpeed = 20f;
-
+    float idleTime = 0.4f;
+    float timer = 0.0f;
     Animator m_Animator;
     Rigidbody m_Rigidbody;
     Vector3 m_Movement;
@@ -29,7 +30,19 @@ public class PlayerMovement : MonoBehaviour
         bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
         bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
-        m_Animator.SetBool("IsWalking", isWalking);
+        if(!isWalking)
+        {
+            timer += Time.deltaTime;
+            if (timer >= idleTime)
+            {
+                m_Animator.SetBool("isWalking", false);
+                timer = 0f;
+            }
+        }else
+        {
+            m_Animator.SetBool("isWalking", true);
+            timer = 0f;
+        }
 
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation(desiredForward);
